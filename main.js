ff11f300334997e3e7965d3ee91f8f7003a59a59
anisda23-meet <anisdai690@gmail.com>
2022-07-25 18:17:45 +0300

    final day 2 Y2

---
+++ main.js
@@ -0,0 +1,10 @@
+function changeBackgroundColor() {
+	if (document.body.style.backgroundColor == 'lightskyblue'){
+		document.body.style.backgroundColor = 'black';
+	}
+
+	else {
+		document.body.style.backgroundColor = 'lightskyblue';
+	}
+
+}
